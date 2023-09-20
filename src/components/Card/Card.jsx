bf0a730ccbca6305199a28054cfc35011a516934
Mario <marioeduardo.garciasalazar@gmail.com>
2023-09-20 16:35:00 -0600

    Edicion de pokedex

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,47 @@
+import "./Card.css";
+
+function Card({ data }) {
+  return (
+    <section className={data.types[0].type.name}>
+      <div>
+        <img src={data.sprites?.other.home.front_default} alt={data.name} />
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>Propiedad</th>
+            <th>Valor</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>ID</td>
+            <td>{data.id}</td>
+          </tr>
+          <tr>
+            <td>Nombre</td>
+            <td>{data.name}</td>
+          </tr>
+          <tr>
+            <td>Altura</td>
+            <td>{data.height}</td>
+          </tr>
+          <tr>
+            <td>Peso</td>
+            <td>{data.weight}</td>
+          </tr>
+          <tr>
+            <td>Experiencia base</td>
+            <td>{data.base_experience}</td>
+          </tr>
+          <tr>
+            <td>Tipo</td>
+            <td>{data.types[0].type.name}</td>
+          </tr>
+        </tbody>
+      </table>
+    </section>
+  );
+}
+
+export default Card;
